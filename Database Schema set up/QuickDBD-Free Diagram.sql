bdbd210c6f48f88953b2a8e71dbaf292a7b42c32
Levreka <90356052+Levreka@users.noreply.github.com>
2022-02-09 18:48:28 -0700

    schema for 1 table/machinelearning

---
+++ Database Schema set up/QuickDBD-Free Diagram.sql
@@ -0,0 +1,15 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/ir7hke
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE `Machinelearning` (
+    `Party` varchar(30)  NOT NULL ,
+    `Gender` varchar  NOT NULL ,
+    `Partisian_score` int  NOT NULL ,
+    `Turnout_scoreint` int  NOT NULL 
+);
+
