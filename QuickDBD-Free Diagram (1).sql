a6004e11157c0a5b1e744545c688ce43561ad01f
Raymond Grymko <r_1rmgaz@hotmail.com>
2022-02-16 17:39:16 -0700

    f

---
+++ QuickDBD-Free Diagram (1).sql
@@ -0,0 +1,40 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/EiljV6
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots (.. - without quotes).
+
+CREATE TABLE Voter (
+    voter_ID int  NOT NULL ,
+    party_Name varchar  NOT NULL ,
+     sex varchar   NOT NULL ,
+    gender varchar   NOT NULL ,
+    age int  NOT NULL ,
+    ethnicity varchar   NOT NULL ,
+    PRIMARY KEY (
+        voter_ID
+    )
+);
+
+CREATE TABLE location_ID (
+    zip int  NOT NULL ,
+    PRIMARY KEY (
+        zip
+    )
+);
+
+CREATE TABLE voting_Scores (
+    voting_Scores int  NOT NULL ,
+    partisan_Score int  NOT NULL ,
+    nat_TO int  NOT NULL ,
+    partisanship_Score int  NOT NULL 
+);
+
+CREATE TABLE civ_Scores (
+    civ_KidsINHH int  NOT NULL ,
+    civ_Libdeo int  NOT NULL ,
+    civ_Marriage int  NOT NULL 
+);
+
---
+++ QuickDBD-Free Diagram (2).sql
@@ -0,0 +1,51 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/EiljV6
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Voter" (
+    "voterID" int   NOT NULL,
+    "partyName" string   NOT NULL,
+    "sex" string   NOT NULL,
+    "age" int   NOT NULL,
+    "ethnicity" string   NOT NULL,
+    CONSTRAINT "pk_Voter" PRIMARY KEY (
+        "voterID"
+     )
+);
+
+CREATE TABLE "InformationID" (
+    "zip" int   NOT NULL,
+    "voterID" int   NOT NULL
+);
+
+CREATE TABLE "votingScores" (
+    "votingScores" int   NOT NULL,
+    "partisanScore" int   NOT NULL,
+    "natTO" int   NOT NULL,
+    "partisanshipScore" int   NOT NULL,
+    "voterID" int   NOT NULL
+);
+
+CREATE TABLE "civScores" (
+    "civScores" int   NOT NULL,
+    "civKidsINHH" int   NOT NULL,
+    "civLibdeo" int   NOT NULL,
+    "civMarriage" int   NOT NULL,
+    "voterID" int   NOT NULL
+);
+
+ALTER TABLE "InformationID" ADD CONSTRAINT "fk_InformationID_voterID" FOREIGN KEY("voterID")
+REFERENCES "Voter" ("voterID");
+
+ALTER TABLE "votingScores" ADD CONSTRAINT "fk_votingScores_voterID" FOREIGN KEY("voterID")
+REFERENCES "Voter" ("voterID");
+
+ALTER TABLE "civScores" ADD CONSTRAINT "fk_civScores_voterID" FOREIGN KEY("voterID")
+REFERENCES "Voter" ("voterID");
+
